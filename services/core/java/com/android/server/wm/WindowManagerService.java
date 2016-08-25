omnirom/android_frameworks_base_old

base: handle button lights the same
build on top of 'lights: Make sure the lights are out'
and handle button light the same

Change-Id: Ic5ca68d07d83e874b38b4f5fd1c2b1b78282d8c4

services/core/java/com/android/server/wm/WindowManagerService.java
@@ -5899,11 +5899,16 @@ public class WindowManagerService extends IWindowManager.Stub
         LightsManager lm = LocalServices.getService(LightsManager.class);
         Light batteryLight = lm.getLight(LightsManager.LIGHT_ID_BATTERY);
         Light notifLight = lm.getLight(LightsManager.LIGHT_ID_NOTIFICATIONS);
+        Light buttonLight = lm.getLight(LightsManager.LIGHT_ID_BUTTON);
+
         if (batteryLight != null) {
             batteryLight.turnOff();
         }
         if (notifLight != null) {
             notifLight.turnOff();
+        }
+        if (buttonLight != null) {
+            buttonLight.turnOff();
         }
         // Make sure the last requested orientation has been applied.
         updateRotationUnchecked(false, false);

iZi-caf/platform_frameworks_base

services/core/java/com/android/server/wm/WindowManagerService.java
@@ -5921,11 +5921,16 @@ public class WindowManagerService extends IWindowManager.Stub
         LightsManager lm = LocalServices.getService(LightsManager.class);
         Light batteryLight = lm.getLight(LightsManager.LIGHT_ID_BATTERY);
         Light notifLight = lm.getLight(LightsManager.LIGHT_ID_NOTIFICATIONS);
+        Light buttonLight = lm.getLight(LightsManager.LIGHT_ID_BUTTONS);
+
         if (batteryLight != null) {
             batteryLight.turnOff();
         }
         if (notifLight != null) {
             notifLight.turnOff();
+        }
+        if (buttonLight != null) {
+            buttonLight.turnOff();
         }
         // Make sure the last requested orientation has been applied.
         updateRotationUnchecked(false, false);