f82e2f7cd7fe7a130f7e2a8e5478ba897bd54ba3
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2016-08-25 01:57:28 +0000

    Marshmallow: configurable advanced reboot menu

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5898,6 +5898,12 @@ public final class Settings {
         public static final String CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED =
                 "camera_double_tap_power_gesture_disabled";
 
+	/**
+         * Whether to include options in power menu for rebooting into recovery or bootloader
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
         /**
          * Beginning of PureNexus Secure Settings Additions
          * @hide
@@ -5966,6 +5972,7 @@ public final class Settings {
             SLEEP_TIMEOUT,
             DOUBLE_TAP_TO_WAKE,
             CAMERA_GESTURE_DISABLED,
+            ADVANCED_REBOOT
         };
 
         /**
